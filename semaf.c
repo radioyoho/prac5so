9008728f16cff2eafc5bf37d36a35cffde3032bc
Dario Hoyo <hoyodario@gmail.com>
2017-10-10 14:23:37 -0500

    Semáforo y cola creadas y puestas en memoria compartida

---
+++ semaf.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/queue.h>
+
+struct queue{
+	int id;
+	TAILQ_ENTRY(queue) entries;		
+};
+
+struct SEMAPHORE {
+	int contador;
+	TAILQ_HEAD (,queue) cola;
+} *s;
+
+
+void waitsem(struct SEMAPHORE);
+void signalsem(struct SEMAPHORE);
+
+void main(){
+	
+	struct queue *item;
+	
+	int shmid = shmget(0x1234,sizeof(shmid),0666|IPC_CREAT);
+	if(shmid==-1)
+	{
+		perror("Error en la memoria compartida\n");
+		exit(1);
+	}
+	
+	s = shmat(shmid,NULL,0);
+	
+	if(s==NULL)
+	{
+		perror("Error en el shmat\n");
+		exit(2);
+	}
+	
+	
+	//----------------------------------END OF THE LINE BOY--------------------------------
+	shmdt(s);
+}
+
+void waitsem(struct SEMAPHORE *sema){
+	sema->
+}
+
+void waitsem(struct SEMAPHORE sema){
+		
+}
