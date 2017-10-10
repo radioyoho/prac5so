198e559a3846938ce639983b4c6ebbbbcb66ae52
Dario Hoyo <hoyodario@gmail.com>
2017-10-10 18:54:28 -0500

    Quedan todos los procesos en pausa

--- semaf.c
+++ semaf.c
@@ -29,20 +29,37 @@ struct SEMAPHORE {
 
 void waitsem(struct SEMAPHORE *);
 void signalsem(struct SEMAPHORE *);
+void initsem(struct SEMAPHORE *);
 
 void proceso(int i)
 	{
+		int l;
 		int k;
 		for(k=0;k<CICLOS;k++)
 		{
+			//printf("Hola\n");
 			// Llamada waitsem implementada en la parte 3
+			//l = 1;
+			//do { atomic_xchg(l,*g); } while(l!=0);
+			
 			waitsem(s);
+			
+			l = 1;
+			*g = 0;
+			
 			printf("Entra %s ",pais[i]);
 			fflush(stdout);
 			sleep(rand()%3);
 			printf("- %s Sale\n",pais[i]);
 			// Llamada waitsignal implementada en la parte 3
+			//l = 1;
+			//do { atomic_xchg(l,*g); } while(l!=0);
+			
 			signalsem(s);
+			
+			l = 1;
+			*g = 0;
+			
 			// Espera aleatoria fuera de la sección crítica
 			sleep(rand()%3);
 		}
@@ -68,7 +85,7 @@ void main(){
 	}
 	//--------------------------------------segunda direccion de memoria compartida---------------------------
 	
-	int shmid2 = shmget(0x1234,sizeof(g),0666|IPC_CREAT);
+	int shmid2 = shmget(0x1235,sizeof(g),0666|IPC_CREAT);
 	if(shmid2==-1)
 	{
 		perror("Error en la memoria compartida\n");
@@ -76,6 +93,8 @@ void main(){
 	}
 	
 	g = shmat(shmid2,NULL,0);
+	
+	*g = 0;
 	
 	if(g==NULL)
 	{
@@ -87,8 +106,8 @@ void main(){
 	int i;
 	int pid;
 	int status;
+	initsem(s);
 	srand(getpid());
-	
 	for(i=0;i<3;i++)
 	{
 		// Crea un nuevo proceso hijo que ejecuta la función proceso()
@@ -104,11 +123,26 @@ void main(){
 	shmdt(g);
 }
 
+void initsem(struct SEMAPHORE *sema){
+	printf("Hola\n");
+	int l = 1;
+	do { atomic_xchg(l,*g);
+	   printf("stuck in init\n");
+	   } while(l!=0);
+	sema->contador = 1;
+	TAILQ_INIT(&sema->cola);
+	*g=0;
+}
+
 void waitsem(struct SEMAPHORE *sema){
 	int l = 1;
 	struct queue *item;
-	do { atomic_xchg(l,*g); } while(l!=0);
+	do { atomic_xchg(l,*g);
+	   printf("stuck in wait\n");
+	   } while(l!=0);
 	sema->contador--;//	
+	printf("Pid: %d\n", getpid());
+	printf("Contador sem: %d\n", sema->contador);
 	if(sema->contador < 0){
 		//poner id de proceso en cola de bloqueado
 		
@@ -122,6 +156,7 @@ void waitsem(struct SEMAPHORE *sema){
 		*g=0;
 		
 		//bloquear proceso
+		printf("Pid: %d", getpid());
 		kill(getpid(), SIGSTOP);
 		//se puede agregar comprobacion	
 	}
@@ -132,16 +167,23 @@ void signalsem(struct SEMAPHORE *sema){
 	int l = 1;
 	struct queue *item;
 	int id;
-	do { atomic_xchg(l,*g); } while(l!=0);
+	do { atomic_xchg(l,*g);
+	   printf("stuck in signal, pid: %d\n", getpid());
+	   } while(l!=0);
+	printf("Contador sem: %d, pid: %d\n", sema->contador, getpid());
 	sema->contador++;
+	printf("Contador sem: %d, pid: %d\n", sema->contador, getpid());
 	if(sema->contador <= 0){
+		printf("Entra a condicion en signal");
 		//quitar un id de la cola
 		item = TAILQ_FIRST(&sema->cola);
 		id = item->id;
 		TAILQ_REMOVE(&sema->cola, item, entries);
 		free(item);
 		//desbloquear ese id
+		printf("%d\n",id);
 		kill(id, SIGCONT);
 	}
+	printf("No entra a condicion en signal");
 	*g=0;
 }
